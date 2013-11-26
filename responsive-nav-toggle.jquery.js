2e905b54ad6c3429040844294508441f14b501fd
Jack Way <jack@nimbleworks.co.uk>
2013-11-26 18:11:31 +0000

    initial commit

---
+++ responsive-nav-toggle.jquery.js
@@ -0,0 +1,24 @@
+(function ($) {
+    'use strict';
+    $(document).ready(function () {
+        $('.nimbleworks-nav').each(function () {
+            var that, navBtn, closedClass, openClass, isOpen, btnClickHandler;
+            that = this;
+            navBtn = $('#' + $(this).attr('data-toggleBtn'));
+            closedClass = 'nimbleworks-nav-closed';
+            openClass = 'nimbleworks-nav-open';
+            btnClickHandler = function (evt) {
+                if (!isOpen && evt.target === navBtn[0]) {
+                    $(that).removeClass(closedClass);
+                    $(that).addClass(openClass);
+                    isOpen = true;
+                } else if (isOpen && !$(that).find(evt.target).length) {
+                    $(that).addClass(closedClass);
+                    $(that).removeClass(openClass);
+                    isOpen = false;
+                }
+            };
+            $(document).click(btnClickHandler);
+        });
+    });
+})(jQuery);
